47b1e7c3fd1bd35114acaaa0bfd3c180412423a5
GUSTAVO SILVA DEODATO <24122736@CFP127ALUNO.LOCAL>
2025-04-10 17:25:49 -0300

    js iniciado

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 "use scrict"
+
+async function Chat(){
+
+    const container = document.getElementById('container-conversas')
+
+    const response = await fetch ("http://localhost:8080/v1/whatssap/usuario/conversas/11987876567")
+    const data = await response.json
+    const conversa = data.data
+
+    conversa.forEach(
+        
+    )
+}
